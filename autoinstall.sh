565341d953e35115efef818a7cc24a5e1e3c626a
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2020-12-30 19:57:38 +0700

    autoinstallsshsl.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -30,6 +30,7 @@ sed -i '$ i\echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6' /etc/rc.local
 # set time GMT +7 jakarta
 ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
 
+
 # set locale
 sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
 /etc/init.d/ssh restart
